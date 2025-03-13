56f097772a954f76e9a4662931e2548af3840265
robinsmith3 <47079647+robinsmith3@users.noreply.github.com>
2025-03-13 18:39:45 +0000

    Delete startup.sh

--- startup.sh
+++
@@ -1,9 +0,0 @@
-# Create a shell script startup.sh to create supporting files and perms
-#!/bin/bash
-
-#/usr/lib/frr/frrinit.sh start
-# Method 1: Run script directly as entrypoint (recommended for interactive)
-#ENTRYPOINT ["/bin/bash", "./checkfile.sh"]
-
-# Method 2: Alternative - Run as command (uncomment to use instead)
-# CMD ["/bin/bash", "./checkfile.sh"]
